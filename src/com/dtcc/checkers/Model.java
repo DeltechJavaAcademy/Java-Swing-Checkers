517e022b910f1ee7ec4d0e6fc375880395c5b364
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-09-15 00:34:53 -0400

    updating files

--- src/com/dtcc/checkers/Model.java
+++ src/com/dtcc/checkers/Model.java
@@ -1,16 +1,138 @@
 package com.dtcc.checkers;
 
-public class Model {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
+public class Model {
+	
+	String [][]board=new String[8][8];
+	
     public String[][] update(Move move){
-        return  null;
+    	if(!Utility.checkProperMove(move,board)) {return board;}
+    	else
+    	{
+    	Utility.setPlayer(board[move.startY][move.startX].charAt(0));
+    	//one diagonal move.
+    	
+    	if(move.endY - move.startY==1 || move.endY -move.startY == -1)
+    	{
+	    	if(board[move.startY][move.startX].equals("R-P"))
+	    	{
+	    		board[move.startY][move.startX]="EMPTY";
+	        	board[move.endY][move.endX]="R-P";
+	    	}
+	    	else if(board[move.startY][move.startX].equals("B-P"))
+	    	{
+	    		board[move.startY][move.startX]="EMPTY";
+	        	board[move.endY][move.endX]="B-P";
+	    	}
+	    	else if(board[move.startY][move.startX].equals("R-K"))
+	    	{
+	    		board[move.startY][move.startX]="EMPTY";
+	        	board[move.endY][move.endX]="R-K";
+	    	}
+	    	else if(board[move.startY][move.startX].equals("B-K"))
+	    	{
+	    		board[move.startY][move.startX]="EMPTY";
+	        	board[move.endY][move.endX]="B-K";
+	    	}
+    	}
+    	
+    	//if a pawn jumps over other pawn.
+    	else if(move.endY - move.startY== 2 || move.endY -move.startY == -2)
+    	{
+    		int newRow=(move.endY + move.startY) / 2;
+    		int newCol= (move.endX + move.startX) / 2;
+    		board[newRow][newCol]="EMPTY";
+    		
+    		if(board[move.startY][move.startX].equals("R-P")) 
+	        { 
+    			board[move.endY][move.endX]="R-P";
+    			board[move.startY][move.startX]="EMPTY";
+	        }
+	    	else if(board[move.startY][move.startX].equals("B-P")) 
+	        {	
+	    		board[move.endY][move.endX]="B-P";
+	    		board[move.startY][move.startX]="EMPTY";
+	        }
+	    	else if(board[move.startY][move.startX].equals("R-K")) 
+	        { 
+    			board[move.endY][move.endX]="R-K";
+    			board[move.startY][move.startX]="EMPTY";
+	        }
+	    	else if(board[move.startY][move.startX].equals("B-K")) 
+	        { 
+    			board[move.endY][move.endX]="B-K";
+    			board[move.startY][move.startX]="EMPTY";
+	        }
+    	}
+    	
+    	//Black pawn turns black king
+    	if (move.endY == 0 && board[move.endY][move.endX].charAt(0) =='B' )
+            //board[move.startY][move.startX] = "B-K";
+    		board[move.endY][move.endX] = "B-K";
+    	
+    	//Red pawn turns red King
+        if (move.endY == 7 &&board[move.endY][move.endX].charAt(0) =='R')
+        	//board[move.startY][move.startX] = "R-K";
+        	board[move.endY][move.endX] = "R-K";
+
+    	return board;
+    	}
     }
 
     public String[][] getBoard() {
-        return null;
+        		//board=new String[8][8];
+        	     for (int row = 0; row < 8; row++) {
+                    for (int col = 0; col < 8; col++) {
+                        if (row % 2 != col % 2 ) {
+                            if (row < 3)
+                                board[row][col] = "R-P";
+                            else if (row > 4)
+                                board[row][col] = "B-P";
+                            else
+                                board[row][col] = "EMPTY";
+                        }
+                        else {
+                            board[row][col] = "EMPTY";
+                        }
+                    }
+                }
+        	return board;
     }
+    
+    public void save(){    
+    
+		FileWriter file=null;
+		try {
+			file = new FileWriter("src/com/dtcc/checkers/SavedBoard14.txt");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		PrintWriter out;
+		try {
+			out = new PrintWriter(file);
+			for(int i = 0; i < board.length; i++)
+			{
+			for(int j = 0; j < board[i].length; j++) {	
+			out.print(board[i][j] + " ");
+				}
+			out.println();
+			}
+			out.flush();
+			out.close();
+		} catch(Exception e) {
+			System.out.println("Exception occured");
+		}
+	}
+	
 
-    public void save(){}
 
     public void load(){}
 }
---
+++ src/com/dtcc/checkers/Utility.java
@@ -0,0 +1,112 @@
+package com.dtcc.checkers;
+
+public class Utility {
+
+	public static char player;
+	public static void print2DArray(String[][] board) {
+		
+		for(int i=0;i<board.length;i++)
+		{
+			for(int j =0;j<board[i].length;j++)
+			{
+				System.out.print(board[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("---------------------------------------------------------------------------------------");
+		
+	}
+	
+	public static boolean checkProperMove(Move move,String[][] board)
+	{
+		boolean isProper=true;
+		if(Utility.getPlayer()== board[move.startY][move.startX].charAt(0))
+		{
+			if(board[move.startY][move.startX].charAt(0)=='R') 
+			{
+				System.out.println("It is other player's turn : Select Black piece."); isProper=false;
+			}
+			else 
+			{
+				System.out.println("It is other player's turn : Select Red piece."); isProper=false;
+			}	
+		}
+		//pawn moving in same column.
+		else if(move.endX - move.startX==0 && (move.endY>=0 && move.endY<=7)){isProper=false; }
+		//else if(board[move.startY][move.startX]) {return false;} //STILL HAVE TO WORK : MORE ILLEGAL moves.
+		else if(move.endY-move.startY >=3 )	//more than 2 squares move. 
+		{
+			System.out.println("Pawn can not take  3 or more squares jump.");
+			isProper=false;
+		}
+		else if(board[move.startY][move.startX].equals("R-P") && move.endY <move.startY )	//Red pawn can not move backward
+		{
+			System.out.println("Red pawn can not go backward");
+			isProper=false;
+		}
+		else if(board[move.startY][move.startX].equals("B-P") && move.endY > move.startY )	//Black pawn can not move backward
+		{
+			System.out.println("Black pawn can not go backward");
+			isProper=false;
+		}
+		else if(!(board[move.endY][move.endX].equals("EMPTY")))	//if square already contains a pawn.
+		{
+			System.out.println("Pawn is present.");
+			return false;
+		}
+		else if(move.endY - move.startY== 2 )	//Red pawn can not jump over same red color pawn.
+		{
+			int x=move.startX;
+			int y =move.endX;
+			if(y-x==2) 
+			{
+				if(board[move.startY][move.startX].charAt(0)=='R' && board[(move.startY)+1][(move.startX)+1].charAt(0)=='R')
+				{
+					System.out.println("Red pawn can not jump over same red color pawn...(right side move)"); return false;
+				}
+			}
+			if(y-x==-2) 
+			{
+				if(board[move.startY][move.startX].charAt(0)=='R' && board[(move.startY)+1][(move.startX)-1].charAt(0)=='R')
+				{
+					System.out.println("Red pawn can not jump over same red color pawn...(left side move)");return false;	
+				}
+			}
+		}
+		else if(move.endY - move.startY== -2 )	//Black pawn can jump over same black color pawn.
+		{
+			
+			int startx=move.startX;
+			int endx =move.endX;
+			if(endx-startx==2) 
+			{
+				if(board[move.startY][move.startX].charAt(0)=='B' && board[(move.startY)-1][(move.startX)+1].charAt(0)=='B')
+				{
+					System.out.println("Black pawn can not jump over same black color pawn....(right side move)"); return false;
+				}
+			}
+			if(endx-startx==-2) 
+			{
+				if(board[move.startY][move.startX].charAt(0)=='B' && board[(move.startY)-1][(move.startX)-1].charAt(0)=='B')
+				{
+					System.out.println("Black pawn can not jump over same black color pawn...(left side move)"); return false;
+				}
+			}
+		}
+		else
+		{
+			isProper=true;
+		}
+		return isProper;
+	}
+	
+	public static void setPlayer(char p)
+	{
+		player=p;
+	}
+	
+	public static char getPlayer()
+	{
+		return player; 
+	}
+}
